41cc351d37165d9c82535c0cd1c7779a82cfa342
Tutlion <assortv@gmail.com>
2024-10-14 17:16:17 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,14 @@
+# 1st programm
+print(9**0.5*5)
+
+# 2st programm
+print(9.99>9.98 and 1000!=1000.1)
+
+# 3st programm
+print(2*2+2)
+print((2+2)*2)
+print(2*2+2 == (2+2)*2)
+
+# 3st programm
+print(int(float('123.456')*10)%10)
+
